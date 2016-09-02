10b489509d16537fe922f91c1fb65b0295331f59
Alec Lownes <alec.lownes@gmail.com>
2016-09-02 13:22:50 -0400

    Add poly overlay to map

--- app/index.jsx
+++ app/index.jsx
@@ -7,22 +7,35 @@ import {GoogleApiWrapper, Map} from 'google-maps-react';
 
 var Container = React.createClass({
   getInitialState: function() {
+    var polyNodes = [
+      {
+        lng:-73.991057,
+        lat: 40.882309
+      },
+      {
+        lng:-73.598320,
+        lat: 40.940642
+      },
+      {
+        lng:-74.090263,
+        lat: 40.558339
+      },
+      {
+        lng:-73.991057,
+        lat: 40.882309
+      }
+    ];
     return {
       trips: [],
       filteredTrips: [],
-      polyNodes: [],
+      polyNodes: polyNodes,
+      polygon: null,
       map: null,
       filters: {}
     };
   },
   componentDidMount: function() {
-    var points = [
-      [-73.991057, 40.882309],
-      [-73.598320, 40.940642],
-      [-74.090263, 40.558339],
-      [-73.991057, 40.882309]
-    ];
-    Network.getAll(this, points, 10000);
+    Network.getAll(this, this.state.polyNodes, 10000);
   },
   render: function() {
     const style = {
@@ -39,12 +52,25 @@ var Container = React.createClass({
           google={window.google}
           onReady={this.getMapReference}
           initialCenter={empireStateBuilding}
+          zoom={11}
           />
       </div>
     );
   },
   getMapReference: function(mapProps, map){
-    this.setState({map: map});
+    var polygon = new window.google.maps.Polygon({
+      paths: this.state.polyNodes,
+      strokeColor: '#0000FF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#0000FF',
+      fillOpacity: 0.35
+    });
+    this.setState({
+      polygon: polygon,
+      map: map
+    });
+    polygon.setMap(map);
   }
 });
 
